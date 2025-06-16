f6dd875bdb52f9601a5b946f2145f47e538d7df9
Harini-1401 <169124118+Harini-1401@users.noreply.github.com>
2025-06-16 23:32:53 +0530

    Base file

---
+++ TimeTwisterApplication.java
@@ -0,0 +1,11 @@
+package com.timetwister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTwisterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTwisterApplication.class, args);
+    }
+}
